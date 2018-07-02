84d3bbba750546a85f26a4667a61f9390d11bdfd
Katherine Lorenzo <katherine.lorenzo@vivoconsulting.com>
2018-07-02 16:57:36 -0600

    modified the view

---
+++ simplyRiskGame/Models/Country.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace simplyRiskGame.Models
+{
+    public class Country
+    {
+        public string name { get; set; }
+
+        public int number { get; set; }
+
+        public List<Country> adjacentCountries { get; set; }
+    }
+}
